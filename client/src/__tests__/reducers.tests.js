d3c77b21ec3ece395b8e4d02e12aa303a7d17a5c
Connor Allen <m.connor.allen@gmail.com>
2022-09-05 08:56:03 -0400

    global store conversion

---
+++ client/src/__tests__/reducers.tests.js
@@ -0,0 +1,8 @@
+// original state
+const state = {
+    name: 'Lerantino',
+    email: 'lerantino@gmail.com'
+}
+
+// update (or mutate) state directly
+const updateState = {...state, email: 'lerantino99@gmail.com'};
